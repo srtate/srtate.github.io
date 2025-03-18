9f76d641042c85612e76ad2dcec15b88868b558c
Steve Tate <srtate@uncg.edu>
2025-03-18 14:35:52 -0400

    Testing actions deployment

--- src/settings.ts
+++ src/settings.ts
@@ -35,7 +35,7 @@ export const template = {
 }
 
 export const seo = {
-	default_title: 'Astro Academia',
+	default_title: 'Home Page - Steve Tate',
 	default_description: 'Astro Academia is a template for academic websites.',
 	default_image: '/images/astro-academia.png',
 }
