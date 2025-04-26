ffcab240de4e110a4f5f2374fd7e08d980789423
Steve Tate <srtate@uncg.edu>
2025-04-26 13:32:06 -0400

    Initial trial upgrade to tailwind 4.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,20 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 
 import { template } from "./src/settings";
 
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind(), sitemap()],
-    site: template.website_url,
-    base: template.base,
+  integrations: [react(), sitemap()],
+  site: template.website_url,
+  base: template.base,
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
