43ce1d234c67d7091a5c00fc788e7471800f72eb
Steve Tate <srtate@uncg.edu>
2025-05-03 17:54:20 -0400

    Major redesign using basic bootstrap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,7 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import react from "@astrojs/react";
-
-import { template } from "./src/settings";
-
-import sitemap from "@astrojs/sitemap";
-
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), sitemap()],
-  site: template.website_url,
-  base: template.base,
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
+  });
