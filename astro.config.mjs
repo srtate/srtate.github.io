55515e99c812d9d7c823cad1fe3364ed5420b9f4
Steve Tate <srtate@uncg.edu>
2026-03-01 12:48:48 +0100

    Added sitemap etc for google

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,22 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [
-			[
-				rehypeKatex,
-				{
-					// Katex plugin options
-				}
-			]
-		]
-	}
+  site: 'https://srtate.github.io/',
+  markdown: {
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [
+          [
+              rehypeKatex,
+              {
+                  // Katex plugin options
+              }
+          ]
+      ]
+	},
 
-  });
+  integrations: [sitemap()]
+});
